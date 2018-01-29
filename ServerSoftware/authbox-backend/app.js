7899f282e50fa50397fa037c230c6fcfdfb7b210
Victor Aprea <victor.aprea@wickeddevice.com>
2018-01-28 19:03:44 -0500

    add v1 mount point for api calls

--- ServerSoftware/authbox-backend/app.js
+++ ServerSoftware/authbox-backend/app.js
@@ -6,8 +6,10 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var homedir = require('homedir')();
+
 var index = require('./routes/index');
-var users = require('./routes/users');
+var v1 = require('./routes/v1');
 
 var app = express();
 app.use(secure);
@@ -25,6 +27,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
+app.use('/v1', v1);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
