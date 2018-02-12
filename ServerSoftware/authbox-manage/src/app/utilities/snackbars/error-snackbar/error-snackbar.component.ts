0c951a8b34478016c0a4561f780d4904dbb0cd40
Victor Aprea <victor.aprea@wickeddevice.com>
2018-02-12 00:01:49 -0500

    add utilities

---
+++ ServerSoftware/authbox-manage/src/app/utilities/snackbars/error-snackbar/error-snackbar.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-error-status-snack',
+  template: '<span class="error-snack">Error! (╯°□°）╯︵ ┻━┻</span>',
+  styleUrls: ['./error-snackbar.component.scss']
+})
+export class ErrorStatusSnackComponent {}
---
+++ ServerSoftware/authbox-manage/src/app/utilities/snackbars/success-snackbar/success-snackbar.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-success-status-snack',
+  template: '<span class="success-snack">Success! ヽ(´▽`)/</span>',
+  styleUrls: ['./success-snackbar.component.scss']
+})
+export class SuccessStatusSnackComponent {}
