9d1e4ec6e66e4d2f153ef3a3b4af40a359afcc95
Victor Aprea <victor.aprea@wickeddevice.com>
2018-02-11 22:02:23 -0500

    add cors to api

--- ServerSoftware/authbox-backend/app.js
+++ ServerSoftware/authbox-backend/app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var cors = require('cors')
 
 var homedir = require('homedir')();
 
@@ -19,6 +20,7 @@ app.set('view engine', 'pug');
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
