aa0e826fb38e41e9b48251dd20331c4416d1b941
Victor Aprea <victor.aprea@wickeddevice.com>
2018-02-13 21:57:21 -0500

    styling adjustments, lightly building out the authbox detail page

--- ServerSoftware/authbox-manage/src/app/authboxes/authbox-detail/authbox-detail.component.ts
+++ ServerSoftware/authbox-manage/src/app/authboxes/authbox-detail/authbox-detail.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
+import { Subscription } from 'rxjs/Subscription';
 import { from } from 'rxjs/observable/from';
 import 'rxjs/add/operator/switchMap';
 
@@ -9,17 +10,19 @@ import 'rxjs/add/operator/switchMap';
   1. The name of the Authbox
   2. Which member has currently authorized it (if any)
   3. A list of members who are currently authorized to use it
-  4. A button to add a new Authbox
+  4. A button to add a new Authorized Member
 */
 
 @Component({
   selector: 'app-authbox-detail',
   templateUrl: './authbox-detail.component.html',
-  styleUrls: ['./authbox-detail.component.css']
+  styleUrls: ['./authbox-detail.component.scss']
 })
-export class AuthboxDetailComponent implements OnInit {
+export class AuthboxDetailComponent implements OnInit, OnDestroy {
 
-  authbox$;
+  private authbox$: Observable<any>;
+  private authboxSub: Subscription;
+  public authboxName;
 
   constructor(
     private route: ActivatedRoute,
@@ -31,6 +34,17 @@ export class AuthboxDetailComponent implements OnInit {
       .switchMap((params: ParamMap) => {
         return from([params.get('id')]);
       });
+
+    this.authboxSub = this.authbox$.subscribe((id) => {
+      this.authboxName = id;
+    });
   }
 
+  ngOnDestroy() {
+    if (this.authboxSub) { this.authboxSub.unsubscribe(); }
+  }
+
+  addAuthorizedMember() {
+
+  }
 }
