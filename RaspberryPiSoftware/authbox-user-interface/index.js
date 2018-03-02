5827a3be28da16028bdb596da394e9e0d7e8f9f3
Victor Aprea <victor.aprea@wickeddevice.com>
2018-03-01 21:53:20 -0500

    added jshint and cleared warnings

--- RaspberryPiSoftware/authbox-user-interface/index.js
+++ RaspberryPiSoftware/authbox-user-interface/index.js
@@ -1,3 +1,7 @@
+/* jshint esversion:6 */
+/* jshint node: true */
+"use strict";
+
 const api = require('./api');
 const lcd = require('./lcd');
 const serial = require('./serial');
@@ -13,9 +17,9 @@ let is_currently_authorized = false;
 // character of input from the user interface
 // which provides the user's access code
 // e.g. keypad, rfid reader, etc
-serial.setInputHandler((chr) => {
+serial.setInputHandler(function(chr) {
   // if the access code ends with '#' reject further
-  // input until it has been processed  
+  // input until it has been processed
   // if the chr is '*' it is the 'backspace' key
   // otherwise append it to the buffered access code
   if(!access_code_buffer.endsWith('#')){
@@ -24,16 +28,26 @@ serial.setInputHandler((chr) => {
       last_access_code_change = moment();
     } else {          // anything else
       access_code_buffer = `${access_code_buffer}${chr}`;
-      last_access_code_change = moment();  
-    }  
+      last_access_code_change = moment();
+    }
+  }
+  else { // otherwise the only two inputs we'll pay attention to are # and *
+         // if we get # in this state it should just extend your authorization
+         // if we get * in this state it should coerce and deauthorization
+    if(chr === '#'){
+      last_access_code_change = moment(); // extend authorization timeout
+    }
+    else if(chr === '*'){
+      // TODO: deauthorize
+    }
   }
 });
 
-const checkForIdleKeypadEntry = () => {
+const checkForIdleKeypadEntry = function() {
   // clear the data entered if it's been idle for too long
-  return new Promise((resolve, reject) => {
+  return new Promise(function(resolve, reject) {
     if(access_code_buffer.length > 0){
-      const automatically_clear_duration_ms = 10 * 1000; 
+      const automatically_clear_duration_ms = 10 * 1000;
       const idle_time_ms = moment().diff(last_access_code_change, 'ms');
       if(idle_time_ms >= automatically_clear_duration_ms){
         access_code_buffer = '';
@@ -43,16 +57,16 @@ const checkForIdleKeypadEntry = () => {
     resolve({
       code: access_code_buffer,
       authorized: is_currently_authorized
-    });  
+    });
   });
-}
+};
 
-const updateLcd = (user) => {  
+const updateLcd = function(user) {
   if(!user.authorized){
     if(user.code){
       const maskedCode = `${user.code.slice(0,-1).replace(/./g,'*')}${user.code.slice(-1)}`;
       return lcd.centerText(maskedCode, 1)
-      .then(() => user);      
+      .then(() => user);
     } else {
       return lcd.centerText(`ENTER CODE`, 1)
       .then(() => user);
@@ -62,27 +76,27 @@ const updateLcd = (user) => {
   }
 };
 
-const validateCode = (user) => {  
+const validateCode = function(user) {
   checkAuthorizedIfReady(user)
-  .then(handleAuthorizationResult); 
+  .then(handleAuthorizationResult);
 };
 
-const checkAuthorizedIfReady = (user) => {
-  return new Promise((resolve, rejetc) => { resolve(user); }) // just to make it a promise result
-  .then((user) => {
+const checkAuthorizedIfReady = function(user) {
+  return new Promise(function(resolve, rejetc) { resolve(user); }) // just to make it a promise result
+  .then(function(user) {
     if(user.code.endsWith('#')){
       if(user.authorized){
-        // if you hit 'enter' and you are authorized 
+        // if you hit 'enter' and you are authorized
         // it will trigger a de-authorization event
         is_currently_authorized = false;
         return Object.assign({}, user, { event: 'deauthorize' });
       } else {
         // not currently authorized so check if we should authorize
-        return db.isAuthorized(access_code_string)
-        .then((isAuthorized) => {
+        return db.isAuthorized(access_code_buffer)
+        .then(function(isAuthorized) {
           is_currently_authorized = isAuthorized;
-          return isAuthorized ? 
-            Object.assign({}, user, { event: 'authorize' }) : 
+          return isAuthorized ?
+            Object.assign({}, user, { event: 'authorize' }) :
             Object.assign({}, user, { event: 'unauthorized' });
         });
       }
@@ -92,13 +106,13 @@ const checkAuthorizedIfReady = (user) => {
   });
 };
 
-const handleAuthorizationResult = (auth) => {
-  return new Promise((resolve, reject) => {
+const handleAuthorizationResult = function(auth) {
+  return new Promise(function(resolve, reject) {
     switch(auth.event){
     case 'authorize':    // was not authorized, now power up
       return serial.authorize()                  // power up the authbox
       .then(api.authorize.bind(null, auth.code)) // register it with the server
-      .then(lcd.authorize);                      // turn the lcd green 
+      .then(lcd.authorize);                      // turn the lcd green
     case 'deauthorize':  // was authorized, now shutting down
       return serial.deauthorize()                // power down the authbox
       .then(api.deauthorize)                     // register it with the server
@@ -107,36 +121,36 @@ const handleAuthorizationResult = (auth) => {
       return lcd.unauthorized();
     }
   })
-  .then(() => {
+  .then(function() {
     if(auth.event){
       access_code_buffer = '';
     }
     return auth;
   });
-}
+};
 
-// interval task to synchronize the local database access codes with the 
+// interval task to synchronize the local database access codes with the
 // no need for this to be in the same thread of control as anything else
-setInterval(() => {
+setInterval(function() {
   api.fetchAccessCodes()
-  .then((codes) => {
+  .then(function(codes) {
     return db.saveAccessCodes(codes);
   })
-  .then(() => {
+  .then(function() {
     console.log(`Database Synchronized @ ${moment().format()}`);
   })
-  .catch((err) => {
+  .catch(function(err) {
     console.err(err);
-  })
+  });
 }, 10 * 60 * 1000 ); // every 10 minutes
 
 serial.begin(); // kick off the serial connection(s)
 
 // an asynchronous non-blocking 'forever' loop
-promiseDoWhilst(() => {
+promiseDoWhilst( function () {
   return checkForIdleKeypadEntry()
   .then(updateLcd)
-  .then(validateCode)
-}, () => {
+  .then(validateCode);
+}, function() {
   return true; // while true
-})
+});
