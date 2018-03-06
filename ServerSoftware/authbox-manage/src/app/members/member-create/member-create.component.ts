7737962d22b087ae24b00bc370b6ba964e3cc9c8
Victor Aprea <victor.aprea@wickeddevice.com>
2018-03-05 22:26:13 -0500

    server and angular updates to support multiple access methods

--- ServerSoftware/authbox-manage/src/app/members/member-create/member-create.component.ts
+++ ServerSoftware/authbox-manage/src/app/members/member-create/member-create.component.ts
@@ -1,5 +1,6 @@
 import { Component, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+import { ApiService } from '../../api.service';
 
 // This will just be a modal dialogue for creating a new Member
 // Needs to collect fields for 'name', 'email' and 'access_code'
@@ -13,9 +14,22 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 })
 export class MemberCreateComponent {
 
+  public access_methods: any = [];
+
   constructor(
+    public apiSrvc: ApiService,
     public dialogRef: MatDialogRef<MemberCreateComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: any) { }
+    @Inject(MAT_DIALOG_DATA) public data: any) {
+
+    this.apiSrvc.getAuthMethods()
+    .then((methods) => {
+      this.access_methods = methods;
+    })
+    .catch((err) => {
+      this.access_methods = [];
+    });
+
+  }
 
   onNoClick(): void {
     this.dialogRef.close();
--- ServerSoftware/authbox-manage/src/app/members/member-edit/member-edit.component.ts
+++ ServerSoftware/authbox-manage/src/app/members/member-edit/member-edit.component.ts
@@ -1,5 +1,6 @@
 import { Component, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+import { ApiService } from '../../api.service';
 
 // This will just be a modal dialogue for editing an existing Members
 @Component({
@@ -8,11 +9,23 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
   styleUrls: ['./member-edit.component.scss']
 })
 export class MemberEditComponent {
-  checked = false;
+  public access_methods: any = [];
+  public checked = false;
 
   constructor(
+    public apiSrvc: ApiService,
     public dialogRef: MatDialogRef<MemberEditComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: any) { }
+    @Inject(MAT_DIALOG_DATA) public data: any) {
+
+    this.apiSrvc.getAuthMethods()
+    .then((methods) => {
+      this.access_methods = methods;
+    })
+    .catch((err) => {
+      this.access_methods = [];
+    });
+
+  }
 
   onNoClick(): void {
     this.dialogRef.close();
@@ -26,4 +39,25 @@ export class MemberEditComponent {
     return Object.assign({}, this.data, {detail: true});
   }
 
+  saveAllowed() {
+    let ret = false;
+
+    if (!this.data.name) { return false; } // name is required
+
+    // either access_code and access_method are both blank
+    // or they are both not blank
+    if ((!!this.data.access_code) !== (!!this.data.access_method)) {
+      return false;
+    } else if (!!this.data.access_code) {
+      // changing an access code / access method is sufficient as long as name is provided
+      ret = true;
+    }
+
+    // changing email is sufficient as long as the above constraints hold
+    if (this.data.email) {
+      ret = true;
+    }
+
+    return ret;
+  }
 }
