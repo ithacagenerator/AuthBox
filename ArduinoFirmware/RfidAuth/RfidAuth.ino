8f0cfabe9ca50f12532aa66cd27840756337e345
Victor Aprea <victor.aprea@wickeddevice.com>
2018-03-10 13:06:55 -0500

    added the support functions for command / control authbox operations

--- ArduinoFirmware/RfidAuth/RfidAuth.ino
+++ ArduinoFirmware/RfidAuth/RfidAuth.ino
@@ -1,3 +1,7 @@
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+
 // Slight adapatation of Wiegand demo code 
 // referenced / linked at http://wiki.seeed.cc/Grove-125KHz_RFID_Reader/
 //
@@ -19,15 +23,22 @@ byte isData1Low = 0;
 int recvBitCount = 0;
 byte isCardReadOver = 0;
 
-void setup()
-{
+void handleCommands(char c);
+uint16_t crc16_update(uint16_t crc, uint8_t a);
+uint16_t crc16(char * str, uint8_t len);
+uint8_t check_crc16(char * str);
+void handleAuthorizeCommand(void);
+void handleLockoutCommand(void);
+
+int LOCKOUT_PIN = 13; // Relay control output
+
+void setup(){
   Serial.begin(115200);
   attachInterrupt(0, ISRreceiveData0, FALLING );  //data0/tx is connected to pin 2, which results in INT 0
   attachInterrupt(1, ISRreceiveData1, FALLING );  //data1/rx is connected to pin 3, which results in INT 1
 }
 
-void loop()
-{
+void loop(){
   //read card number bit
   if(isData0Low||isData1Low){
     if(1 == recvBitCount){//even bit
@@ -60,6 +71,10 @@ void loop()
     }
     resetData();
   }
+
+  while (Serial.available()){
+      handleCommands(Serial.read());
+  }  
 
 }
 
@@ -119,4 +134,100 @@ void ISRreceiveData1(){
   isData1Low = 1;
 }
 
+uint16_t crc16_update(uint16_t crc, uint8_t a){
+    crc ^= a;
+    for (uint8_t i = 0; i < 8; ++i){
+        if (crc & 1)
+            crc = (crc >> 1) ^ 0xA001;
+        else
+            crc = (crc >> 1);
+    }
 
+    return crc;
+}
+
+uint16_t crc16(char * str, uint8_t len){
+    uint16_t crc = 0;
+    for(uint16_t ii = 0; ii < len; ii++){
+        crc = crc16_update(crc, str[ii]);
+    }
+    return crc;
+}
+
+uint8_t check_crc16(char * str){
+    uint8_t ret = 0;
+    uint8_t len = strlen(str);
+    if(len >= 5){
+        uint16_t calculated_crc16 = crc16(str, len - 4);
+        char * temp;        
+        uint16_t expected_crc16 = strtol(str + strlen(str) - 4, &temp, 16); // base 16
+        if (*temp == NULL){
+            if(calculated_crc16 == expected_crc16){
+                ret = 1; // success!
+            }
+        }
+    }
+    return ret;
+}
+
+// state machine for handline incoming characters from the Raspberry Pi
+// commands are always terminated by XXXX\n, 
+// where XXXX is a CRC16 of the bytes in the command as a HEX-string and \n is a newline character
+// CRC16 is computed on the byte values in the line up to the checksum
+// CRC16 implementation is explicitly included as a function below
+// taking the algorithm from http://www.nongnu.org/avr-libc/user-manual/group__util__crc.html
+// this should make it pretty difficult to "accidentally" send an authorization command
+// though it's admittedly kind of overkill when the commands are not parametric
+// should be relatively easy to extend this command processing logic to support more commands
+//
+// valid commands are:
+//   authorize40A2\n
+//   lockoutDF6B\n
+//
+void handleCommands(char c){
+    static char lineBuffer[64] = {0}; // allow up to 63 characters in a command (plus null terminator)
+    static uint8_t idx = 0;
+    if(c == '\n' || c == '\r'){
+        // newline triggers processing of the command, but only if the checksum is sound
+        // remember the last four characters in the buffer are expected to be a hex value string
+        if(check_crc16(lineBuffer)){
+            lineBuffer[strlen(lineBuffer) - 4] = '\0'; // don't need the checksum anymore
+            if(0 == strcmp_P(lineBuffer, PSTR("authorize"))){
+                handleAuthorizeCommand();
+            }
+            else if(0 == strcmp_P(lineBuffer, PSTR("lockout"))){
+                handleLockoutCommand();
+            }
+            else {
+              Serial.print(F("Unknown Command: \"")); Serial.print(lineBuffer); Serial.print(F("\""));Serial.println();              
+            }
+        }
+        else{
+            Serial.print(F("Invalid CRC16: \"")); Serial.print(lineBuffer); Serial.print(F("\""));Serial.println();
+        }
+
+        idx = 0; // get ready for next line
+        memset(lineBuffer, 0, sizeof(lineBuffer));        
+    }
+    else{
+        // buffer the character
+        lineBuffer[idx] = c;
+
+        // advance the write index, the highest value idx should ever hold is
+        // 62 for a 64 byte buffer, because 63 must always hold a null terminator
+        // if you would overflow, just keep overwriting the last possible buffer entry
+        if(idx < (sizeof(lineBuffer) - 1)){
+            idx++;
+        }
+    }
+}
+
+void handleAuthorizeCommand(void){    
+    Serial.println("authorize");
+    digitalWrite(LOCKOUT_PIN, HIGH);
+}
+
+void handleLockoutCommand(void){    
+    Serial.println("lockout");    
+    digitalWrite(LOCKOUT_PIN, LOW);
+}
