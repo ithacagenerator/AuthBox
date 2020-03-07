67fb261e4444c352973e31dcb274733f01f1946c
Victor Aprea <victor.aprea@wickeddevice.com>
2020-03-07 18:09:04 -0500

    debug

--- ServerSoftware/authbox-backend/routes/v1.js
+++ ServerSoftware/authbox-backend/routes/v1.js
@@ -863,6 +863,7 @@ router.get('/authboxes/history/:authboxName/:secret', (req, res, next) => {
 });
 
 function namifyMember(period, member) {
+  const _period = moment(period);
 
   if (!member) {
     return {};
@@ -990,7 +991,6 @@ function namifyMember(period, member) {
 
   // Also decorate the member with their likely membership tier based on payment amount
   // do so by looking at the last payment record that is in or before this month
-  const _period = moment(period);
   _period.date(1).hours(0).minutes(0).seconds(0).milliseconds(0);
   const allPayments = member.paypal.filter(v => {
     if (!v._m && v.payment_date) {
