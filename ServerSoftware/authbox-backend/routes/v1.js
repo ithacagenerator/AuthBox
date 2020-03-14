4d519d2dba67af1bdb67da8029acdd31f5733d93
Victor Aprea <victor.aprea@wickeddevice.com>
2020-03-14 17:41:25 -0400

    fix active

--- ServerSoftware/authbox-backend/routes/v1.js
+++ ServerSoftware/authbox-backend/routes/v1.js
@@ -1078,7 +1078,7 @@ router.get('/members/active/:secret', async (req, res, next) => {
     });
 
     res.json(
-      activeMembers.map(namifyMember.bind(null, moment().format('YYYY-MM')))
+      activeMembers.map(namifyMember.bind(null, moment()))
     );
   } catch (e) {
     console.error(e.message || e, e.stack);
